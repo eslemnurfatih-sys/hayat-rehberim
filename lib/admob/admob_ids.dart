c6dbbaf85d8cdf7f05cbf52bf51a005e297acd60
eslemnurfatih-sys <eslemnurfatih@gmail.com>
2026-03-24 22:10:27 +0300

    Create admob_ids.dart file with Google AdMob test IDs

---
+++ lib/admob/admob_ids.dart
@@ -0,0 +1,12 @@
+// Google AdMob Test IDs
+
+class AdMobIds {
+  static const String bannerAdUnitId = "ca-app-pub-3940256099942544/6300978111"; // Test Banner Ad ID for Android
+  static const String interstitialAdUnitId = "ca-app-pub-3940256099942544/1033173712"; // Test Interstitial Ad ID for Android
+  static const String rewardedAdUnitId = "ca-app-pub-3940256099942544/5224354917"; // Test Rewarded Ad ID for Android
+
+  // Test IDs for iOS
+  static const String iosBannerAdUnitId = "ca-app-pub-3940256099942544/2934735716"; // Test Banner Ad ID for iOS
+  static const String iosInterstitialAdUnitId = "ca-app-pub-3940256099942544/4411468910"; // Test Interstitial Ad ID for iOS
+  static const String iosRewardedAdUnitId = "ca-app-pub-3940256099942544/1712485313"; // Test Rewarded Ad ID for iOS
+}
