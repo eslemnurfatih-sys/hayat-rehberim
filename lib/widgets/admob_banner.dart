ce55568b29f4499f530f964c2823eeb479dece56
eslemnurfatih-sys <eslemnurfatih@gmail.com>
2026-03-25 21:34:40 +0300

    Add BannerAd widget for displaying ads at the bottom of the app

---
+++ lib/widgets/admob_banner.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+class BannerAdWidget extends StatefulWidget {
+  @override
+  _BannerAdWidgetState createState() => _BannerAdWidgetState();
+}
+
+class _BannerAdWidgetState extends State<BannerAdWidget> {
+  late BannerAd _bannerAd;
+
+  @override
+  void initState() {
+    super.initState();
+    _bannerAd = BannerAd(
+      adUnitId: '<YOUR_AD_UNIT_ID>', // Replace with your ad unit ID
+      request: AdRequest(),
+      listener: BannerAdListener(
+        onAdLoaded: (_) {
+          setState(() {});
+        },
+        onAdFailedToLoad: (ad, error) {
+          ad.dispose();
+          print('Failed to load a banner ad: $error');
+        },
+      ),
+    )..load();
+  }
+
+  @override
+  void dispose() {
+    _bannerAd.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: _bannerAd != null
+          ? AdWidget(ad: _bannerAd)
+          : SizedBox(height: 50),
+      height: 50,
+    );
+  }
+}
